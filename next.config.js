81e450ad0f863905979a68b365448206dffbe76d
Thiago Pereira Perez <tpperez@gmail.com>
2021-05-02 06:16:11 -0300

    some adjustments

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const { PHASE_EXPORT } = require("next/constants")
 const withPlugins = require("next-compose-plugins")
 const optimizedImages = require("next-optimized-images")
 const path = require("path")
 
 const nextConfig = {
-  [PHASE_EXPORT]: { exportTrailingSlash: true },
   poweredByHeader: false,
   devIndicators: {
     autoPrerender: false,
